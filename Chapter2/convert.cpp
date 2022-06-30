7c01e35c0ace49e0f54c3056b59146d5923208c3
mwang0517 <53281582+mwang0517@users.noreply.github.com>
2022-06-30 00:22:42 -0230

    2.6

---
+++ Chapter2/convert.cpp
@@ -0,0 +1,19 @@
+//2.6
+#include <iostream>
+int stonetolb(int);
+
+int main() {
+    
+    using namespace std;
+    int stone;
+    cout << "Enter the weight in stone: ";
+    cin >> stone;
+    int pounds = stonetolb(stone);
+    cout << stone << " stone = ";
+    cout << pounds << " pounds = ";
+    return 0;
+}
+
+int stonetolb(int sts) {
+    return 14 * sts;
+}
