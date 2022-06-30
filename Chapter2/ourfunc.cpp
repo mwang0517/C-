d6c02dce48ffb91c9b90efd5b880a16bc8bf1cc3
mwang0517 <53281582+mwang0517@users.noreply.github.com>
2022-06-30 00:15:53 -0230

    2.5

---
+++ Chapter2/ourfunc.cpp
@@ -0,0 +1,15 @@
+//2.5
+#include <iostream>
+void simon(int);
+
+int main() {
+    
+    using namespace std;
+    simon(3);
+    return 0;
+}
+
+void simon(int n) {
+    using namespace std;
+    cout << "Simon says touch your toes " << n << " times." << endl;
+}
