4f26b55339e2ebf5c68f594e575bcd210da3eddf
mwang0517 <53281582+mwang0517@users.noreply.github.com>
2022-06-30 00:10:42 -0230

    2.4

---
+++ Chapter2/sqrt.cpp
@@ -0,0 +1,15 @@
+//2.4
+#include <iostream>
+#include <cmath>
+
+int main() {
+    
+    using namespace std;
+    double area;
+    cout << "Enter the floor area, in square feet, of your home: ";
+    cin >> area;
+    double side;
+    side = sqrt(area);
+    cout << "That is the equivalent of a square " << side << " feet to the side." << endl;
+    return 0;
+}
