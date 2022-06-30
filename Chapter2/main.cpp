b89ada68f491fdc0b47373a4f24b6baf83b2eead
mwang0517 <53281582+mwang0517@users.noreply.github.com>
2022-06-29 23:59:21 -0230

    2.1

--- Chapter2/main.cpp
+++ Chapter2/main.cpp
@@ -1,6 +1,11 @@
+//2.1
 #include <iostream>
 
-int main(){
-    std::cout << "Hello Testing" << std::endl;
+
+int main() {
+    
+    using namespace std;
+    cout << "Come Up and c++ me some time." << endl;
+    cout << "You won't regret it." << endl;
     return 0;
 }
