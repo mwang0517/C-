a3a618390c9f17e765d4f961ae1966071ba141b7
mwang0517 <53281582+mwang0517@users.noreply.github.com>
2022-07-02 23:15:14 -0230

    4.11

---
+++ Chapter4/structur.cpp
@@ -0,0 +1,28 @@
+//4.11
+#include <iostream>
+
+struct inflatable{
+    char name[20];
+    float volume;
+    double price;
+};
+
+int main() {
+    using namespace std;
+    inflatable guest = {
+        "Glorious Gloria",
+        1.88,
+        29.99
+    };
+    inflatable pal ={
+        "Audacious Arthur",
+        3.12,
+        32.99
+    };
+    
+    cout << "Expand yout guest list with " << guest.name;
+    cout << " and " << pal.name << endl;
+    cout << guest.price + pal.price << endl;
+    return 0;
+    
+}
