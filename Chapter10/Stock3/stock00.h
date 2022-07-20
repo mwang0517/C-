f7ffa316207f580056e35ea75c73686f9191c090
mwang0517 <53281582+mwang0517@users.noreply.github.com>
2022-07-20 16:08:35 -0230

    10.7&10.8

---
+++ Chapter10/Stock3/stock00.h
@@ -0,0 +1,25 @@
+#ifndef STOCK00_H_
+#define STOCK00_H_
+
+#include <string>
+
+class Stock{
+private:
+    std::string company;
+    long shares;
+    double share_val;
+    double total_val;
+    void set_tot(){total_val = share_val * shares;}
+public:
+    Stock(); //default constructor
+    Stock(const std::string & co, long n = 0, double pr = 0.0);
+    ~Stock(); //noisy constructor
+    void acquire(const std::string & co, long n, double pr);
+    void buy(long num, double price);
+    void sell(long num, double price);
+    void update(double price);
+    void show() const;
+    const Stock & topval(const Stock & s) const;
+};
+
+#endif
