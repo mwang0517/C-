93dbf81fa3e478d0206c8c6b278e2718ae87289f
mwang0517 <53281582+mwang0517@users.noreply.github.com>
2022-07-23 14:44:58 -0230

    11.19&11.20&11.21

---
+++ Chapter11/stonewt1/stonewt.h
@@ -0,0 +1,23 @@
+#ifndef STONEWT_H_
+#define STONEWT_H_
+
+class Stonewt{
+    
+private:
+    enum {Lbs_per_stn = 14};
+    int stone; //whole stones
+    double pds_left; //fractional pounds
+    double pounds; //entire weight in pounds
+
+public:
+    Stonewt(double lbs);
+    Stonewt(int stn, double lbs);
+    Stonewt();
+    ~Stonewt();
+    void show_lbs() const;
+    void show_stn() const;
+    //conversion functions
+    operator int() const;
+    operator double() const;
+};
+#endif
